505281e10daeb143fc155cbdb91e33ffa96ca082
aambrosio22 <aambrosio22@fed.idserve.net>
2022-12-07 12:36:30 +0100

    Initial Commit

---
+++ ARc/ARcApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARcApp.swift
+//  ARc
+//
+//  Created by Antonia Ambrosio on 07/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ARcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
